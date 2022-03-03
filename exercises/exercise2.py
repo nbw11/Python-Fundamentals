f9360dcb9c1302f067bbdb2424eaf7372f53b05b
Nicholas Willis <nbw11@live.com>
2022-03-03 15:25:33 -0600

    Exercise 2

---
+++ exercises/exercise2.py
@@ -0,0 +1,38 @@
+# 1.
+def simple():
+    value1, value2, value3, value4, value5 = "red" , "blue" , "green", "black", "white"
+    print(value1)
+    print(value2)
+    print(value3)
+    print(value4)
+    print(value5)
+
+simple()
+
+# 2.
+def simple2():
+    size = 'small'
+    print(size)
+    size = 'medium'
+    print(size)
+    size = 'large'
+    print(size)
+    size = 'extra large'
+    print(size)
+
+
+
+simple2()
+
+# 3.
+
+def message(arg1):
+    print(arg1)
+
+message('howdy yall')
+
+
+
+
+
+
