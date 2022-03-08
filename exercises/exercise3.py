4a8aa28f0dc0137aa2c9f8f8aca412ad21c0f002
Nicholas Willis <nbw11@live.com>
2022-03-07 18:50:12 -0600

    Exercise 3

---
+++ exercises/exercise3.py
@@ -0,0 +1,28 @@
+# 2-3 Personal Message
+name_1 = "Eric"
+message = "Hello {}, would you like to learn some Python today?"
+print(message.format(name_1))
+
+# 2-4 Name Cases
+name_2 = "Nick"
+print(name_2.upper())
+print(name_2.lower())
+print(name_2.title())
+
+# 2-5 Famous Quote
+message = '"Our greatest glory is not in never falling, but in rising every time we fall." - Batman'
+print(message)
+
+# 2-6 Famous Quote 2
+famous_person = "Batman"
+message = '"Our greatest glory is not in never falling, but in rising every time we fall." - {}'
+print(message.format(famous_person))
+
+# 2-7 Stripping Names
+name_3 = " Morty "
+print(name_3.lstrip())
+print(name_3.rstrip())
+print(name_3.strip())
+
+
+
