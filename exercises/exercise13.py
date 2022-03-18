d6a2ffbdcafad35e06799382a1720fa441f0b976
Nicholas Willis <nbw11@live.com>
2022-03-18 15:37:18 -0500

    exercise 13

---
+++ exercises/exercise13.py
@@ -0,0 +1,45 @@
+# 1. 8-15, 8-16
+# Start with some designs that need to be printed
+
+import boat as mn
+from boat import boat_float_sink as fn
+from boat import *
+print(mn.my_boat)
+print(fn)
+
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+# Simulate printing each design, until none are left.
+# Move each design to completed_models after printing.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"Printing model: {current_design}")
+    completed_models.append(current_design)
+# Display all completed models.
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
+
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
+
+# 2
+
+import restaurant
+print(restaurant.Restaurant.describe_restaurant())
